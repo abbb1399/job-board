337c645cf84b67efe999f7ea554540d7b23959c6
paul <abbb123@naver.com>
2025-06-22 15:35:31 +0900

    feat: SideBar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import {
+  Sidebar,
+  SidebarHeader,
+  SidebarProvider,
+  SidebarContent,
+  SidebarTrigger,
+  SidebarFooter,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar";
+import { AppSidebarClient } from "./_AppSidebarClient";
+
 export default function HomePage() {
-  return <h1>hi</h1>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">WDS Jobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>asdf</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">asdf</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
