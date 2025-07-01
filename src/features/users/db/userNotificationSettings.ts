a6680552ff5acb9aeab8565cab3c753c7eaddafd
paul <abbb123@naver.com>
2025-07-01 22:26:50 +0900

    feat: 캐싱 시스템

--- src/features/users/db/userNotificationSettings.ts
+++ src/features/users/db/userNotificationSettings.ts
@@ -1,11 +1,14 @@
 import { db } from "@/drizzle/db";
 import { UserNotificationSettingsTable } from "@/drizzle/schema";
+import { revalidateUserNotificationSettingsCache } from "@/features/users/db/cache/userNotificationSettings";
 
 export async function insertUserNotificationSettings(
   settings: typeof UserNotificationSettingsTable.$inferInsert
 ) {
-  return await db
+  await db
     .insert(UserNotificationSettingsTable)
     .values(settings)
     .onConflictDoNothing();
+
+  revalidateUserNotificationSettingsCache(settings.userId);
 }
--- src/features/users/db/users.ts
+++ src/features/users/db/users.ts
@@ -1,18 +1,25 @@
 import { db } from "@/drizzle/db";
 import { UserTable } from "@/drizzle/schema";
+import { revalidateUserCache } from "@/features/users/db/cache/users";
 import { eq } from "drizzle-orm";
 
 export async function insertUser(user: typeof UserTable.$inferInsert) {
-  return await db.insert(UserTable).values(user).onConflictDoNothing();
+  await db.insert(UserTable).values(user).onConflictDoNothing();
+
+  revalidateUserCache(user.id);
 }
 
 export async function updateUser(
   id: string,
   user: Partial<typeof UserTable.$inferSelect>
 ) {
-  return await db.update(UserTable).set(user).where(eq(UserTable.id, id));
+  await db.update(UserTable).set(user).where(eq(UserTable.id, id));
+
+  revalidateUserCache(id);
 }
 
 export async function deleteUser(id: string) {
-  return await db.delete(UserTable).where(eq(UserTable.id, id));
+  await db.delete(UserTable).where(eq(UserTable.id, id));
+
+  revalidateUserCache(id);
 }
